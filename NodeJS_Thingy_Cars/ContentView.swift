8d89709340607f2281e1ddbd5ac3bbf8414bc689
sc4n1a471 <martin.terhes@icloud.com>
2022-06-30 23:10:55 +0200

    Working GET

--- NodeJS_Thingy_Cars/ContentView.swift
+++ NodeJS_Thingy_Cars/ContentView.swift
@@ -6,15 +6,135 @@
 //
 
 import SwiftUI
+//import Foundation
+
+//final class Car: Codable {
+//    enum CodingKeys: CodingKey {
+//        case license_plate
+//        case brand
+//        case model
+//        case codename
+//        case year
+//        case comment
+//    }
+//
+//    @Published var license_plate = "AAA111"
+//    @Published var brand = "Default Brand"
+//    @Published var model = "Default Model"
+//    @Published var codename = "Default Codename"
+//    @Published var year = 1900
+//    @Published var comment = "Default Comment"
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//
+//        license_plate = try container.decode(String.self, forKey: .license_plate)
+//        brand = try container.decode(String.self, forKey: .brand)
+//        model = try container.decode(String.self, forKey: .model)
+//        codename = try container.decode(String.self, forKey: .codename)
+//        year = try container.decode(Int.self, forKey: .year)
+//        comment = try container.decode(String.self, forKey: .comment)
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//
+//        try container.encode(license_plate, forKey: .license_plate)
+//        try container.encode(brand, forKey: .brand)
+//        try container.encode(model, forKey: .model)
+//        try container.encode(codename, forKey: .codename)
+//        try container.encode(year, forKey: .year)
+//        try container.encode(comment, forKey: .comment)
+//    }
+//}
+
+struct Response: Codable {
+    var status: String
+    var message: [Cars]
+}
+
+struct Cars: Codable {
+    var license_plate: String
+    var brand: String
+    var model: String
+    var codename: String?
+    var year: Int?
+    var comment: String?
+}
+
+struct Response_failed: Codable {
+    var status: String
+    var message: String
+}
+
+//struct Test {
+//    var name: String
+//    var id = UUID()
+//}
 
 struct ContentView: View {
+    @State private var results = [Cars]()
+
     var body: some View {
-        Group {
-            Form {
-                Text("he")
-                Text("nyeh")
+        NavigationView {
+            List(results, id: \.license_plate) { car in
+                VStack(alignment: .leading) {
+                    Text(car.license_plate)
+                        .font(.headline)
+                    HStack {
+                        Text(car.brand)
+                        Text(car.model)
+                        Text(car.codename ?? "")
+                    }
+                    
+                }
+            }
+            
+            .task {
+                await loadData()
             }
         }
+        .navigationTitle("Cars")
+        
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "http://192.168.5.175/cars") else {
+            print("Invalid URL")
+            return
+        }
+//        print("URL: \(url)")
+        
+        do {
+            // (data, metadata)-ban metadata most nem kell, ezért lehet _
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+//            print("data: \(String(describing: String(data: data, encoding: .utf8)))")
+            
+            initData(dataCuccli: data)
+        } catch {
+            print("Invalid data")
+        }
+    }
+    
+    func initData(dataCuccli: Data) {
+        var decodedData: Response
+        do {
+            decodedData = try JSONDecoder().decode(Response.self, from: dataCuccli)
+                
+//            print("decodedData: \(decodedData)")
+                
+            if (decodedData.status == "success") {
+                print("status: \(decodedData.status)")
+                results = decodedData.message
+            } else {
+                print("Failed response: \(decodedData.message)")
+            }
+    
+        } catch {
+            print(error)
+        }
+        
     }
 }
 
