567e9be1eb8b59ef1e274928645beaae83d267a7
sc4n1a471 <martin.terhes@icloud.com>
2022-06-30 19:28:05 +0200

    First

--- NodeJS_Thingy_Cars/ContentView.swift
+++ NodeJS_Thingy_Cars/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Group {
+            Form {
+                Text("he")
+                Text("nyeh")
+            }
         }
     }
 }
